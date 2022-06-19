abcc16ed351590de9610b4a7dfa0a26a146e0d0d
Nicolas de Queiroz Cipriano <79324236+NicolasUnivesp@users.noreply.github.com>
2022-06-19 00:06:00 -0300

    Update base.py
    inserção do host http://espacondaleitura.azurewebsites.net/ na linha 29

--- Biblioteca/settings/base.py
+++ Biblioteca/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)4+ddamzu%n)wb47+85gmtkd9pj^(x@7uz*bfod=xj__&8g#^(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://espacondaleitura.azurewebsites.net/']
 
 
 # Application definition
